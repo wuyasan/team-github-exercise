c88ee96fd63447a4cffbefdf5f99b44470c72447
bluemailbox <77300788+bluemailbox@users.noreply.github.com>
2022-09-21 12:46:24 -0700

    main class and avg feature

---
+++ arrayMain.java
@@ -0,0 +1,7 @@
+public class arrayMain {
+    public static void main(String[] args) {
+        int y[] = {4,12,4,5};
+        System.out.println((arrayAvg.getAvg(y)));
+        //additional methods to be created by team
+    }
+}
