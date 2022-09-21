8971fa3917af13aef9a6434de821a6284263a9a1
bluemailbox <77300788+bluemailbox@users.noreply.github.com>
2022-09-21 13:04:37 -0700

    User input array

--- arrayMain.java
+++ arrayMain.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class arrayMain {
     public static void main(String[] args) {
-        int y[] = {4,12,4,5};
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int size = in.nextInt();
+        int[] y = new int[size];
+        for(int i = 0; i < size; i++) {
+            System.out.println("Enter number for array: ");
+            y[i] = in.nextInt();
+        }
+        
         System.out.println((arrayAvg.getAvg(y)));
         //additional methods to be created by team
+        in.close();
     }
 }
